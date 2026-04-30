7af28ea242a6de67eeac5333f7716717e9e87c93
Tanishk Panchal <Tanineymar@users.noreply.github.com>
2026-04-30 23:17:02 +0530

    Modal UI done

--- frontend/src/components/AuthModal.jsx
+++ frontend/src/components/AuthModal.jsx
@@ -1,36 +1,105 @@
-import loginImg from '../assets/login.jpg'
-import { X } from 'lucide-react'
+import { useRef, useState } from 'react'
+import API from '../api/axios.js'
+import axios from 'axios'
+import { X } from "lucide-react"
+import AuthImg from '../assets/login.jpg'
 
-function AuthModal() {
+function AuthModal({ onClose, activeModal }) {
+
+    const [name, setName] = useState("")
+    const [signEmail, setSignEmail] = useState("")
+    const [signPassword, setSignPassword] = useState("")
+
+    const [loginEmail, setLoginEmail] = useState("")
+    const [loginPassword, setLoginPassword] = useState("")
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const timeRef = useRef(null)
+
+
+    if (!activeModal) {
+        return null
+    }
+
+    // Register API
+
+    const handleRegister = async (event) => {
+        event.preventDefault()
+        setLoading(true)
+        try {
+            const response = await axios.post("/auth/signup", {
+                name,
+                email: signEmail,
+                password: signPassword,
+                role: "patient"
+            })
+            onClose()
+        } catch (error) {
+            message: "Registration failed",
+                console.error(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    // Login API
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        setLoading(true)
+        try {
+            const response = await axios.post('/auth/login', {
+                email: loginEmail,
+                password: loginPassword
+            })
+            onClose()
+        } catch (error) {
+            message: "Login failed",
+                console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
         <>
-            <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-xs p-8'>
+            <div onClick={() => onClose()} className=' fixed inset-0 z-50 backdrop-blur-xs bg-black/40 ' />
 
-                {/* Modal container */}
-
-                <div className='relative w-full h-auto max-w-4xl bg-white rounded-2xl shadow-xl overflow-hidden flex flex-col sm:flex-row'>
-                    <button className='absolute top-4 right-4 z-10 p-2 rounded-full bg-white/80 hover:bg-gray-100'>
+            <div className='fixed inset-0 z-50 flex items-center justify-center p-4'>
+                <div className='relative w-full max-w-3xl bg-white rounded-2xl shadow-2xl overflow-hidden flex flex-col sm:flex-row' >
+                    <button onClick={() => onClose()} className=' absolute right-4 top-4 p-1.5 rounded-full bg-white/80 hover:bg-red-100 transition-colors'>
                         <X className='w-5 h-5 text-gray-600' />
                     </button>
-                    {/* Left img */}
-                    <div className='hidden sm:block sm:w-1/2 h-48 sm:h-auto'>
-                        <img src={loginImg} alt='img' className='w-full h-full object-cover' />
+
+                    <div className='hidden sm:block w-1/2'>
+                        <img src={AuthImg}
+                            className='w-full h-full object-cover'
+                        />
                     </div>
-                    {/* right side */}
-                    <div className='w-full sm:w-1/2 p-6 sm:p-10 flex flex-col text-gray-800'>
-                        <h2 className='text-2xl sm:text-3xl font-semibold mb-2 text-gray-800'>Sign up</h2>
-                        <p className='text-sm text-gray-600 mb-4'>Create your account to manage appointments and lab tests in one place.</p>
-                        <input className='p-2 w-full border border-gray-300 rounded-lg mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" placeholder='Enter Full Name' />
-                        <input className='w-full border border-gray-300 rounded-lg p-2 mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500' type="email" placeholder='Enter Email' />
-                        <input className='w-full border border-gray-300 rounded-lg p-2 mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500' type="password" placeholder='Enter password' />
-                        <button className="w-full bg-blue-600 hover:bg-blue-700 transition text-white py-2.5 rounded-lg font-medium">
-                            Continue
-                        </button>
-                           
+
+                    <div className='w-full sm:w-1/2 p-8 flex flex-col' >
+                        <h1 className='text-2xl font-bold text-blue-600 mb-1' >Medi<span className='text-teal-500'>Care</span></h1>
+                        <div className='flex border-b border-slate-100 mb-5 mt-4'>
+                            <button onClick={() => onClose("login")} className={`flex-1 pb-2.5 text-sm 
+                                font-semibold border-b-2 transition-all ${activeModal === "login" ? "border-blue-600 text-blue-600" : "border-transparent text-slate-400"}
+                                `}>
+                                Login
+                            </button>
+                            <button onClick={() => onClose("signup")} className={`flex-1 pb-2.5 text-sm font-semibold border-b-2 transition-all ${activeModal === "signup" ? "border-blue-600 text-blue-600" : "border-transparent text-slate-400"
+                                }`}>
+                                Sign up
+                            </button>
+                        </div>
+
                     </div>
+
+
+
                 </div>
             </div>
         </>
+
     )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,32 +2,39 @@
 import { useState } from "react"
 import AuthModal from "./AuthModal"
 
-function Navbar(){
-    const [activeModal , setActiveModal]=useState("")
-    return(
+function Navbar() {
+    const [activeModal, setActiveModal] = useState("")
+    return (
         <>
-        <nav className="flex justify-between items-center px-8 py-4 border-b-olive-700 bg-white shadow-md  ">
-            <h1 className="text-xl font-bold text-blue-600">Medi<span className="text-teal-500">Care</span></h1>
+            <nav className="flex justify-between items-center px-8 py-4 border-b-olive-700 bg-white shadow-md  ">
+                <h1 className="text-xl font-bold text-blue-600">Medi<span className="text-teal-500">Care</span></h1>
 
-            <div className=" hiden md:flex items-center gap-7 text-slate-500 font-medium ">
-                <a href="#" className=" hover:text-blue-600 transition-colors">Home</a>
-                <a href="#" className=" hover:text-blue-600 transition-colors">Doctors</a>
-                <a href="#" className=" hover:text-blue-600 transition-colors">Lab Tests</a>
-                <a href="#" className=" hover:text-blue-600 transition-colors">Contact</a>
-            </div>
+                <div className=" hiden md:flex items-center gap-7 text-slate-500 font-medium ">
+                    <a href="#" className=" hover:text-blue-600 transition-colors">Home</a>
+                    <a href="#" className=" hover:text-blue-600 transition-colors">Doctors</a>
+                    <a href="#" className=" hover:text-blue-600 transition-colors">Lab Tests</a>
+                    <a href="#" className=" hover:text-blue-600 transition-colors">Contact</a>
+                </div>
 
-           <div className="flex gap-2.5">
-             <button onClick={()=>setActiveModal("login")} 
-              className="text-sm font-semibold text-blue-600 px-4 py-2 border border-blue-200 bg-blue-50 rounded-xl hover:bg-blue-100  transition-colors">Login</button>
-            <button
-            onClick={()=>setActiveModal("register")}
-             className="bg-blue-600 text-white text-sm font-semibold px-4 py-2 rounded-xl hover:bg-blue-700 transition-colors ">Register</button>
-           </div>
-        </nav>
-        <AuthModal
-        activeModal = {activeModal}
-        onClose={()=> setActiveModal("")}
-        />
+                <div className="flex gap-2.5">
+                    <button onClick={() => setActiveModal("login")}
+                        className="text-sm font-semibold text-blue-600 px-4 py-2 border border-blue-200 bg-blue-50 rounded-xl hover:bg-blue-100  transition-colors">Login</button>
+                    <button
+                        onClick={() => setActiveModal("signup")}
+                        className="bg-blue-600 text-white text-sm font-semibold px-4 py-2 rounded-xl hover:bg-blue-700 transition-colors ">Signup</button>
+                </div>
+            </nav>
+
+            <AuthModal
+                activeModal={activeModal}
+                onClose={(switchTo) => {
+                    if (switchTo) {
+                        setActiveModal(switchTo)  
+                    } else {
+                        setActiveModal("")         
+                    }
+                }}
+            />
         </>
     )
 }
