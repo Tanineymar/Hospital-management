e1962da6d5216d8e8a5dff7c43d03ad0efbd29a4
Tanishk Panchal <Tanineymar@users.noreply.github.com>
2026-05-03 16:40:22 +0530

    Hero section done

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,10 +4,10 @@
 function Hero() {
   return (
     <>
-      <section className="grid md:grid-cols-2 gap-8 px-6 py-12 bg-blue-50">
+      <section className="grid md:grid-cols-2 gap-15 px-6 py-12 bg-blue-50">
         {/* Left */}
         <div>
-          <span className="text-xs bg-blue-100 px-3 py-1 rounded-full text-blue-600 font-semibold">
+          <span className="text-xs bg-blue-200 px-3 py-1 rounded-full text-blue-600 font-semibold">
             Trusted by 10,000+ patients
           </span>
           <h1 className="text-3xl md:text-5xl font-semibold  mt-4">
@@ -27,17 +27,49 @@ function Hero() {
           </div>
         </div>
         {/* Right Card */}
-
         <div className="flex justify-center">
-          <div className="bg-white p-5 rounded-xl w-70 shadow-2xl shadow-blue-900 h">
-            <div className="bg-blue-100 w-10 h-10 p-2 rounded-full flex justify-center items-center font-semibold text-blue-700">
-              AK
+          <div className="w-full max-w-sm bg-white rounded-2xl shadow-2xl shadow-blue-900 ">
+            <div className="flex items-center gap-4  bg-blue-950 p-5 rounded-t-2xl">
+              <div className="w-14 h-14 rounded-full bg-blue-300 flex items-center justify-center font-bold text-blue-700 ">
+                AM
               </div>
-              
+
+              <div>
+                <h2 className="font-semibold text-lg text-white">
+                  Dr. Arjun Mehta
+                </h2>
+                <p className="text-sm text-blue-300  font-semibold ">
+                  Cardiologist
+                </p>
+              </div>
+            </div>
+            {/* Experience + Fee */}
+            <div className="flex justify-between text-sm bg-slate-200 p-6">
+              <div>
+                <p className="font-semibold text-gray-800">14 yrs</p>
+                <p className="text-gray-700 text-xs">Experience</p>
+              </div>
+              <div>
+                <p className="font-semibold text-gray-800">₹800</p>
+                <p className="text-gray-700 text-xs">Fee</p>
+              </div>
+            </div>
+
+            {/* buttons */}
+            <div className="flex gap-2 p-5">
+            <button className="flex-1 py-2 rounded-xl border text-sm text-gray-600 hover:bg-gray-50 transition font-semibold">
+              View Profile
+            </button>
+
+            <button className="flex-1 py-2 rounded-xl bg-blue-950 text-white text-sm hover:bg-blue-900 transition font-semibold">
+              Book Appointment
+            </button>
+          </div>
 
           </div>
         </div>
-        
+
+
       </section>
     </>
   )
