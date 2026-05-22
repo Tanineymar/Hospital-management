49d44d29764d6dcf06333ee96156b5173bead7a8
Tanishk Panchal <Tanineymar@users.noreply.github.com>
2026-05-22 14:11:05 +0530

    Doctor dashboard get profile done

--- frontend/src/pages/doctor/Profile.jsx
+++ frontend/src/pages/doctor/Profile.jsx
@@ -17,7 +17,7 @@ function Field({ icon: Icon, label, value }) {
 }
 
 function Profile() {
-  const [Profile, setProfile] = useState(null)
+  const [profile, setProfile] = useState(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -36,7 +36,7 @@ function Profile() {
 
   if (loading) {
     return (
-      <div className='flex justify-center items-center text-center '>
+      <div className='flex justify-center items-center h-[90vh] '>
         Loading your profile...
       </div>
     )
@@ -54,19 +54,104 @@ function Profile() {
         <div className='flex justify-between py-5'>
           <div className=' flex items-center gap-4 mb-8'>
             <div className='w-16 h-16 rounded-2xl bg-blue-600 text-white flex items-center text-2xl justify-center font-bold'>
-              {Profile?.user?.name?.charAt(0)}
+              {profile?.user?.name?.charAt(0)}
             </div>
             {/* /Info */}
             <div>
-            <h2 className='text-xl font-bold text-slate-800'>{Profile?.user?.name}</h2>
-            <p className='text-sm text-slate-600'>{Profile?.specialization}</p>
-            <p className='text-xs text-slate-500'>{Profile?.user?.email}</p>
-          </div>
+              <h2 className='text-xl font-bold text-slate-800'>{profile?.user?.name}</h2>
+              <p className='text-sm text-slate-600'>{profile?.specialization}</p>
+              <p className='text-xs text-slate-500'>{profile?.user?.email}</p>
+            </div>
           </div>
 
+          {/* Availability */}
+          <div className='flex items-center gap-4 mb-8 px-1'>
+            <p className='text-sm  font-semibold text-slate-600'>Available</p>
+            <div
+              className={`w-14 h-7 rounded-full relative
+  
+              ${profile?.isAvailable
+                  ? "bg-green-500"
+                  : "bg-slate-300"
+                }
+              `}
+            >
+
+              <div
+                className={`absolute top-1 w-5 h-5 bg-white rounded-full transition-all
+      
+              ${profile?.isAvailable
+                    ? "left-8"
+                    : "left-1"
+                  }
+              `}
+              />
+
+            </div>
+
+            <span
+              className={`text-sm font-medium
+    
+            ${profile?.isAvailable
+                  ? "text-green-600"
+                  : "text-slate-400"
+                }
+             `}
+            >
+
+              {
+                profile?.isAvailable
+                  ? "Online"
+                  : "Offline"
+              }
+
+            </span>
+
+          </div>
+
+
+        </div>
+
+        {/* fields */}
+        <div className='grid grid-cols-2 gap-5 '>
+          <Field
+            icon={User} label="Full Name"
+            value={profile?.user?.name}
+          />
+          <Field
+            icon={Mail} label="Email"
+            value={profile?.user?.email}
+          />
+          <Field
+            icon={Stethoscope} label="Speacialization"
+            value={profile?.specialization}
+          />
+          <Field
+            icon={GraduationCap} label="Qualification"
+            value={profile?.qualification}
+          />
+          <Field
+            icon={Clock} label="Experience"
+            value={`${profile?.experience}`}
+          />
+          <Field
+            icon={IndianRupee} label="Consultation Fee"
+            value={`₹ ${profile?.consultationFee}`}
+          />
+          <Field
+            icon={BadgeCheck} label="License Number"
+            value={profile?.licenseNumber}
+          />
           
         </div>
 
+        {/* About */}
+        <div className='mt-6'>
+          <Field 
+           icon={FileText} label="About"
+           value={profile?.about}
+          />
+        </div>
       </div>
 
     </div>
